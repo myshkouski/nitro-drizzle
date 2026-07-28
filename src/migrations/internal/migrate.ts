d71392ce4cec87d65f2536f62df00647e22a9242
Alexei Myshkouski <alexeimyshkouski@gmail.com>
2026-07-29 01:20:53 +0300

    chore(migrations): remove debug logging from migration execution

--- src/migrations/internal/migrate.ts
+++ src/migrations/internal/migrate.ts
@@ -25,7 +25,6 @@ export async function migrateDatabase<TSession = any>(
 ) {
   for await (const { filename, idx, ...migrationsMeta } of migrations) {
     try {
-      console.debug(config);
       await database.dialect.migrate([migrationsMeta], database.session, {
         migrationsSchema: config?.schema,
         migrationsTable: config?.table,
